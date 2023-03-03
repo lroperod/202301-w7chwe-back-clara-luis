5ae33303d3d5fcb4a7781494b2667be3b7a143fc
luis <luisrym@gmail.com>
2023-03-03 18:36:39 +0100

    create app router and test

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import apiRouter from './api/api-router.js';
+import cors from 'cors';
+import authRouter from './api/auth/auth-router.js';
+
+const app = express();
+
+app.use(cors());
+
+app.disable('x-powered-by');
+app.get('/', (req, res) => {
+  res.json('Server is working!!');
+});
+
+app.use(express.json());
+
+app.use('/auth', authRouter);
+app.use('/api/v1', apiRouter);
+
+export default app;
